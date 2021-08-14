75bd238c21ed94397e7ef80679872d78ef6f97be
ChansooKim316 <cccssk@naver.com>
2021-08-14 20:02:59 +0900

    err handling

--- main.py
+++ main.py
@@ -102,14 +102,16 @@ def compare():
    default_url_1 = 'https://static01.nyt.com/images/2019/06/17/science/17DOGS/17DOGS-mobileMasterAt3x-v2.jpg'
    default_url_2 = 'https://i.guim.co.uk/img/media/684c9d087dab923db1ce4057903f03293b07deac/205_132_1915_1150/master/1915.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=14a95b5026c1567b823629ba35c40aa0'
 
-   # Get images from given URLs
+      # Get images from given URLs
    if request.method == 'POST':
-      url1 = request.form['url1']
-      url2 = request.form['url2']
-      image1 = requests.get(url1, stream = True) 
-      image2 = requests.get(url2, stream = True) 
-      # print('got URLs')
-   
+      try:
+         url1 = request.form['url1']
+         url2 = request.form['url2']
+         image1 = requests.get(url1, stream = True) 
+         image2 = requests.get(url2, stream = True) 
+      except Exception as err:
+         return "<h3>Something went wrong... Please try URLs that start with 'https://...' </h3> \n"
+
 
       if image1.status_code == 200 and image2.status_code == 200:
          # To avoid image file's size being zero
@@ -119,7 +121,7 @@ def compare():
             shutil.copyfileobj(image1.raw, f)
          with open('images/image2','wb') as f:
             shutil.copyfileobj(image2.raw, f)
-      # print('downloaded images')
+      print('downloaded images')
       
       # Read images
       try:
@@ -127,13 +129,13 @@ def compare():
          img2 = Image.open('images/image2')
       except Exception as err:
          return "<h3>Something went wrong... Please try other URLs.</h3> \n"
-      # print('read images')
+      print('read images')
 
       # Calculate similarity
       hash1 = imagehash.average_hash(img1)
       hash2 = imagehash.average_hash(img2)
       similarity = 100 - (hash1 - hash2)
-      # print('similarity :')
+      print('similarity :')
       # print(similarity)
 
       return '''
@@ -141,6 +143,7 @@ def compare():
             <form action="http://localhost:5000/compare">
                <input type="submit" value="Go back" />
             </form>
+            <a href="javascript:history.back()">BACK</a>
             '''.format(similarity)
 
    return '''
